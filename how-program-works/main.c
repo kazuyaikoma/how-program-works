c2de9a0b24a8844cfe1827a32192c950a81e6892
nixiesquid <audu817@gmail.com>
2019-08-20 17:49:31 +0900

    implement calcByReplacing

--- how-program-works/main.c
+++ how-program-works/main.c
@@ -10,13 +10,15 @@
 #include <string.h>
 
 void researchDecimalNumber(float);
+void calcByReplacing(int);
 
 int main() {
-    researchDecimalNumber(0.1);
+//    researchDecimalNumber(0.75);
+    calcByReplacing(10);
     return 0;
 }
 
-// 単精度浮動小数点数で、引数に渡された任意の10進数がどのように表現されているか調べる
+// Find out how any decimal number passed as an argument is represented as a single precision floating point number
 void researchDecimalNumber(float f) {
     // float: 4バイトの単精度浮動小数点実数 (有効桁数は7ケタ)
     // double: 8バイトの倍精度浮動小数点実数 (有効桁数は16ケタ)
@@ -47,4 +49,19 @@ void researchDecimalNumber(float f) {
     s[34] = '\0';
     
     printf("%s\n", s);
+}
+
+// Calculate by replacing decimals with integers
+void calcByReplacing(int count) {
+    if (count <= 1) { return; }
+    
+    float sum = 0;
+    int i;
+    
+    for (i=1;i<=count;i++) {
+        sum += 1;
+    }
+    sum /= 10;
+    
+    printf("%f\n", sum);
 }
